324eb9abd6c3c6663a531575545282afc2af7ca4
LeonardoFreitas <leonardofr.rocha@hotmail.com>
2023-05-13 17:31:08 -0300

    update/creation of Categories and Articles controllers and models

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const app = express();
 const bodyParser = require("body-parser");
 const connection = require("./database/database");
 
+const categoriesController = require("./categories/CategoriesController");
+const articlesController = require("./articles/ArticlesController");
+
 // View engine
 app.set("view engine", "ejs");
 
@@ -21,6 +24,10 @@ connection
     }).catch((error) => {
         console.log(error);
     })
+
+// tells express to use the routes defined in the controller
+app.use("/", categoriesController);
+app.use("/", articlesController);
 
 app.get("/", (req, res) => {
     res.render("index");
